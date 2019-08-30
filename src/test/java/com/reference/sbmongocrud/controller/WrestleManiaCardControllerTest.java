cec4b5213ff644e0d32d738be93889b61760b040
amyth <lalwani.amith@gmail.com>
2019-08-30 02:47:17 -0500

    add inital junit skeleton test

---
+++ src/test/java/com/reference/sbmongocrud/controller/WrestleManiaCardControllerTest.java
@@ -0,0 +1,14 @@
+package com.reference.sbmongocrud.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WrestleManiaCardControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
